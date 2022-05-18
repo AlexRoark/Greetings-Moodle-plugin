18cb8a5d775bc4feb7124bfa600c5edc3777a855
Alex <hurricane.insight@gmail.com>
2022-05-18 17:02:46 +0300

    Save and display greeting messages

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_greetings';
-$plugin->release = '0.1.0';
-$plugin->version = 2022051400;
+$plugin->release = 'Carrot';
+$plugin->version = 2022051403;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
