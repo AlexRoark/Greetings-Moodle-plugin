699e91bd0c5c26a438d160cd3f291d45443a0e90
Alex <hurricane.insight@gmail.com>
2022-05-18 18:33:38 +0300

    Login and user input sanity check

--- lib.php
+++ lib.php
@@ -63,14 +63,17 @@ function local_greetings_get_greeting($user)
  */
 function local_greetings_extend_navigation_frontpage(navigation_node $frontpage)
 {
+    if (!isguestuser()):
     $frontpage->add(
         get_string('pluginname', 'local_greetings'),
         new moodle_url('/local/greetings/index.php')
     );
+    endif;
 }
 
 function local_greetings_extend_navigation(global_navigation $root)
 {
+    if (!isguestuser()):
     $node = navigation_node::create(
         get_string('pluginname', 'local_greetings'),
         new moodle_url('/local/greetings/index.php')
@@ -78,4 +81,5 @@ function local_greetings_extend_navigation(global_navigation $root)
 
     $node->showinflatnavigation = true;
     $root->add_node($node);
+    endif;
 }
